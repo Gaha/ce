ddcdcb9ed7eff04975423ee8e4c96b56106b54ab
Gaha <vdesessard@gmail.com>
2013-09-02 01:26:36 +0200

    Ajouter des activitee

--- ce.py
+++ ce.py
@@ -13,49 +13,57 @@ import sqlite3
 from tableTexte import Table
 
 class CE(object) :
-	"""
-	Créé un rapport
-	"""
-	def __init__(self) :
-		# création de la base de données
-		self.bdd = sqlite3.connect('bddCE.sqlite')
-		self.cursor = self.bdd.cursor()
-		# création des tables
-		self.cursor.execute('CREATE TABLE IF NOT EXISTS Agent (nom text primary key, prenom text)')
-		self.cursor.execute('CREATE TABLE IF NOT EXISTS Commission (nom text primary key)')
-		self.cursor.execute('CREATE TABLE IF NOT EXISTS Activitee (nom text, idcommission integer, date timestamp)')
-		#self.cursor.execute('CREATE TABLE IF NOT EXISTS evenement (id integer primary key, idrapport integer, famille text, genre text, message text)')
-		#self.cursor.execute('CREATE TABLE If NOT EXISTS rapport (datecreation timestamp primary key DEFAULT current_timestamp, nom text)')
+    """
+    Créé un rapport
+    """
+    def __init__(self) :
+        # création de la base de données
+        self.bdd = sqlite3.connect('bddCE.sqlite')
+        self.cursor = self.bdd.cursor()
+        # création des tables
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Agent (nom text primary key, prenom text)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Commission (nom text primary key)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Activitee (nom text, idcommission integer, date timestamp DEFAULT current_timestamp)')
+        #self.cursor.execute('CREATE TABLE IF NOT EXISTS evenement (id integer primary key, idrapport integer, famille text, genre text, message text)')
+        #self.cursor.execute('CREATE TABLE If NOT EXISTS rapport (datecreation timestamp primary key DEFAULT current_timestamp, nom text)')
 
-	def agent_ajout(self, nom, prenom) :
-		"""
-		Ajout un agent à la liste
-		"""
-		self.cursor.execute("INSERT into Agent (nom, prenom) values('" + nom + "','" + prenom + "')")
+    def agent_ajout(self, nom, prenom) :
+        """
+        Ajout un agent à la liste
+        """
+        self.cursor.execute("INSERT into Agent (nom, prenom) values('" + nom + "','" + prenom + "')")
 
-	def agent_liste(self) :
-		"""
-		Liste les agents
-		"""
-		self.cursor.execute("SELECT * from Agent")
-		return self.cursor.fetchall()
+    def agent_liste(self) :
+        """
+        Liste les agents
+        """
+        self.cursor.execute("SELECT * from Agent")
+        return self.cursor.fetchall()
 
-	def commission_ajout(self, nom) :
-		self.cursor.execute("INSERT into Commission (nom) values ('" + nom + "')")
+    def commission_ajout(self, nom) :
+        self.cursor.execute("INSERT into Commission (nom) values ('" + nom + "')")
 		
-	def commission_liste(self) :
-		self.cursor.execute("SELECT * from Commission")
-		return self.cursor.fetchall()
+    def commission_liste(self) :
+        self.cursor.execute("SELECT * from Commission")
+        return self.cursor.fetchall()
 	
-	def activitee_ajout(self, nom, commission, date) :
-		# on recupère l'ID de la commission
-		self.cursor.execute("SELECt rowid from Commission where nom = '" + commission +"'")
-		try :
-			idcommission = self.cursor.fetchone()[0]
-		except TypeError :
-			raise TypeError("Commission inconnut")
-		
-		
+    def activitee_ajout(self, nom, commission) :
+        # on recupère l'ID de la commission
+        self.cursor.execute("SELECT rowid from Commission where nom = '" + commission +"'")
+        try :
+            idcommission = self.cursor.fetchone()[0]
+        except TypeError :
+            raise TypeError("Commission inconnut")
+        self.cursor.execute("INSERT into activitee (nom, idcommission) values ('" + nom + "'," + str(idcommission) + ")")
+	
+    def activitee_liste(self) :
+        self.cursor.execute("SELECT ac.nom, co.nom, ac.date from Activitee ac INNER JOIN Commission co ON ac.idcommission = co.rowid")
+        return self.cursor.fetchall()
+    
+    def activitee_synthese(self) :
+        self.cursor.execute("SELECT co.nom, count(ac.idcommission) as Total from Activitee ac inner join Commission co ON ac.idcommission = co.rowid group by co.nom")
+        return self.cursor.fetchall()
+        
 	"""
     def rapport(self, nom) :
         #try :
@@ -107,30 +115,20 @@ class CE(object) :
     def evenement_nouveau(self, idrapport, famille, genre, message = "") :
         self.cursor.execute("INSERT INTO evenement VALUES(null," + str(idrapport) + ",'" + str(famille) + "','" + genre + "','" + message +"')")
 	"""
-	def __del__(self):
-		self.bdd.commit()
-		self.cursor.close()
+    def __del__(self):
+        self.bdd.commit()
+        self.cursor.close()
 
 if __name__ == '__main__':
-	base = CE()
-	base.agent_liste()
-	print Table("AGENT", ["Nom","Prénom"], base.agent_liste())
-	print
+    base = CE()
+    base.agent_liste()
+    print Table("AGENT", ["Nom","Prénom"], base.agent_liste())
+    print
 
-	print Table("COMMISSION", ["Nom"], base.commission_liste())
-
-	base.activitee_ajout("test","Sport","date")
-
-    #print("+-----------------------+")
-    #print("|       FAMILLE         |")
-    #print("+=======================+")
-    #for famille in base.famille_liste() :
-    #    print("| " + famille[0].ljust(22) + "|")
-    #print("+=======================+")
-    #print
-
-    #print Table("EVENEMENT", ["ID","Rapport","Famille","Genre","Message"], base.rapport_evenement())
-    #print
+    print Table("COMMISSION", ["Nom"], base.commission_liste())
+    print
     
-    #print Table("SYNTHESE", ["Rapport","Famille","Genre","Nombre"], base.rapport_synthese())
-    #print
+    print Table("ACTIVITEE", ["Nom","Commission","Date"], base.activitee_liste())
+    print
+    
+    print Table("ACTIVITEE SYNTHESE", ["Commission","Nombre"], base.activitee_synthese())
